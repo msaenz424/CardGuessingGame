fc2b751848bfab7f41711c33f4932fa919b5aa03
Miguel Saenz <msaenz424@gmail.com>
2016-09-02 19:22:47 -0700

    Initial commit

---
+++ app/src/main/java/com/example/miguel/guessingplayingcard/FragmentCards.java
@@ -0,0 +1,20 @@
+package com.example.miguel.guessingplayingcard;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Miguel on 8/22/2016.
+ */
+public class FragmentCards extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_cards, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/miguel/guessingplayingcard/Game.java
@@ -0,0 +1,64 @@
+package com.example.miguel.guessingplayingcard;
+
+/**
+ * Created by Miguel on 8/22/2016.
+ */
+public class Game {
+    private int mCounter = 0;
+    private static final int CARDS_IN_GAME = 21; //for this to work it has to be an odd number and multiple of 3
+    public static final int CARDS_IN_EACH_COLUMN = CARDS_IN_GAME / 3;
+    private int[] cardsArray;
+
+
+    public Game(){
+        cardsArray = new int[] {
+                R.drawable.c101,
+                R.drawable.c102,
+                R.drawable.c103,
+                R.drawable.c104,
+                R.drawable.c105,
+                R.drawable.c106,
+                R.drawable.c107,
+                R.drawable.c108,
+                R.drawable.c109,
+                R.drawable.c110,
+                R.drawable.c111,
+                R.drawable.c112,
+                R.drawable.c113,
+                R.drawable.c114,
+                R.drawable.c115,
+                R.drawable.c116,
+                R.drawable.c117,
+                R.drawable.c118,
+                R.drawable.c119,
+                R.drawable.c120,
+                R.drawable.c121
+        };
+    }
+
+    /*
+    Each array argument represents a column of playing cards
+    Method re-arranges playing cards and returns a new array of them.
+    c counts the total of amount of playing cards
+    i counts cards in each column
+     */
+    public void arrangeNewCardsArray(int[] chosenColumn, int[] firstColumn, int[] lastColumn){
+        int c = 0;
+        for (int i = 0; i < CARDS_IN_EACH_COLUMN; i++){
+            cardsArray[c] = firstColumn[i];
+            c++;
+        }
+        for (int i = 0; i < CARDS_IN_EACH_COLUMN; i++){
+            cardsArray[c] = chosenColumn[i];
+            c++;
+        }
+        for (int i = 0; i < CARDS_IN_EACH_COLUMN; i++){
+            cardsArray[c] = lastColumn[i];
+            c++;
+        }
+    }
+
+    public int[] getCardsArray(){
+        return cardsArray;
+    }
+}
