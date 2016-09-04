e180b110d7d3f4260a4493f688d4853749d6b8c2
Miguel Saenz <msaenz424@gmail.com>
2016-09-04 12:29:13 -0700

    Added fragmentCard to MainActivity

--- app/src/main/java/com/example/miguel/guessingplayingcard/FragmentCards.java
+++ app/src/main/java/com/example/miguel/guessingplayingcard/FragmentCards.java
@@ -15,6 +15,8 @@ public class FragmentCards extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_cards, container, false);
+        View view;
+        view = inflater.inflate(R.layout.fragment_cards,container,false);
+        return view;
     }
 }
--- app/src/main/java/com/example/miguel/guessingplayingcard/MainActivity.java
+++ app/src/main/java/com/example/miguel/guessingplayingcard/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.miguel.guessingplayingcard;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // attaches "gvCards" gridview to the fragment
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        FragmentCards fragmentCards = new FragmentCards();
+        fragmentTransaction.add(R.id.gvCards,fragmentCards);
+        fragmentTransaction.commit();
+
     }
 }
