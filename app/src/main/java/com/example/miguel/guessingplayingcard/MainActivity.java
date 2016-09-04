3a4e230135aecbed6757357a187951886daf5bc8
Miguel Saenz <msaenz424@gmail.com>
2016-09-04 13:10:58 -0700

    corrected right fragment container

--- app/src/main/java/com/example/miguel/guessingplayingcard/MainActivity.java
+++ app/src/main/java/com/example/miguel/guessingplayingcard/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // shows gridview filled with cards
+        GridView gridview = (GridView) findViewById(R.id.gvCards);
+        gridview.setAdapter(new ImageAdapter(this));
+
         // attaches "gvCards" gridview to the fragment
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         FragmentCards fragmentCards = new FragmentCards();
-        fragmentTransaction.add(R.id.gvCards,fragmentCards);
+        fragmentTransaction.add(R.id.fragment_container,fragmentCards);
         fragmentTransaction.commit();
-
     }
 }
