e5ec5ef233a4b7ac679e37bcd818cd2f4f591064
Miguel Saenz <msaenz424@gmail.com>
2016-09-10 18:45:12 -0700

    added shuffling functionality to deck of cards

--- app/src/main/java/com/example/miguel/guessingplayingcard/Game.java
+++ app/src/main/java/com/example/miguel/guessingplayingcard/Game.java
@@ -1,8 +1,9 @@
 package com.example.miguel.guessingplayingcard;
 
-/**
- * Created by Miguel on 8/22/2016.
- */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Game {
     int mCounter=0;
     private static final int CARDS_IN_GAME = 21; //for this to work it has to be an odd number and multiple of 3
@@ -10,33 +11,69 @@ public class Game {
     private int cardsArray[][] = new int[CARDS_IN_EACH_COLUMN][3];
 
     public Game(){
-        int[] tempArray = {
-                R.drawable.c101,
-                R.drawable.c102,
-                R.drawable.c103,
-                R.drawable.c104,
-                R.drawable.c105,
-                R.drawable.c106,
-                R.drawable.c107,
-                R.drawable.c108,
-                R.drawable.c109,
-                R.drawable.c110,
-                R.drawable.c111,
-                R.drawable.c112,
-                R.drawable.c113,
-                R.drawable.c114,
-                R.drawable.c115,
-                R.drawable.c116,
-                R.drawable.c117,
-                R.drawable.c118,
-                R.drawable.c119,
-                R.drawable.c120,
-                R.drawable.c121
-        };
+        List<Integer> arrayDeck = new ArrayList<>();
+        arrayDeck.add( R.drawable.c101);
+        arrayDeck.add( R.drawable.c102);
+        arrayDeck.add( R.drawable.c103);
+        arrayDeck.add( R.drawable.c104);
+        arrayDeck.add( R.drawable.c105);
+        arrayDeck.add( R.drawable.c106);
+        arrayDeck.add( R.drawable.c107);
+        arrayDeck.add( R.drawable.c108);
+        arrayDeck.add( R.drawable.c109);
+        arrayDeck.add( R.drawable.c110);
+        arrayDeck.add( R.drawable.c111);
+        arrayDeck.add( R.drawable.c112);
+        arrayDeck.add( R.drawable.c113);
+        arrayDeck.add( R.drawable.c114);
+        arrayDeck.add( R.drawable.c115);
+        arrayDeck.add( R.drawable.c116);
+        arrayDeck.add( R.drawable.c117);
+        arrayDeck.add( R.drawable.c118);
+        arrayDeck.add( R.drawable.c119);
+        arrayDeck.add( R.drawable.c120);
+        arrayDeck.add( R.drawable.c121);
+        arrayDeck.add( R.drawable.c122);
+        arrayDeck.add( R.drawable.c123);
+        arrayDeck.add( R.drawable.c124);
+        arrayDeck.add( R.drawable.c125);
+        arrayDeck.add( R.drawable.c126);
+        arrayDeck.add( R.drawable.c127);
+        arrayDeck.add( R.drawable.c128);
+        arrayDeck.add( R.drawable.c129);
+        arrayDeck.add( R.drawable.c130);
+        arrayDeck.add( R.drawable.c131);
+        arrayDeck.add( R.drawable.c132);
+        arrayDeck.add( R.drawable.c133);
+        arrayDeck.add( R.drawable.c134);
+        arrayDeck.add( R.drawable.c135);
+        arrayDeck.add( R.drawable.c136);
+        arrayDeck.add( R.drawable.c137);
+        arrayDeck.add( R.drawable.c138);
+        arrayDeck.add( R.drawable.c139);
+        arrayDeck.add( R.drawable.c140);
+        arrayDeck.add( R.drawable.c141);
+        arrayDeck.add( R.drawable.c142);
+        arrayDeck.add( R.drawable.c143);
+        arrayDeck.add( R.drawable.c144);
+        arrayDeck.add( R.drawable.c145);
+        arrayDeck.add( R.drawable.c146);
+        arrayDeck.add( R.drawable.c147);
+        arrayDeck.add( R.drawable.c148);
+        arrayDeck.add( R.drawable.c149);
+        arrayDeck.add( R.drawable.c150);
+        arrayDeck.add( R.drawable.c151);
+        arrayDeck.add( R.drawable.c152);
+        arrayDeck.add( R.drawable.c153);
+        arrayDeck.add( R.drawable.c154);
+        arrayDeck.add( R.drawable.c155);
+
+        Collections.shuffle(arrayDeck);
         int c = 0;
         for (int i = 0; i < CARDS_IN_EACH_COLUMN; i++){
             for (int j = 0; j < 3; j++){
-                cardsArray[i][j] = tempArray[c++];
+                cardsArray[i][j] = arrayDeck.get(c);
+                c++;
             }
         }
     }
